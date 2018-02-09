69dd2de51b1d83d02ff94e57bf44113d8fb404f1
Patricia Limcangco <plimcangco1@studentmail.nait.ca>
2018-02-08 20:46:31 -0700

    Update custom.js

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -3,12 +3,7 @@ $(".menu-toggle").click(function(e) {
     $("#sidebar-wrapper").toggleClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     $(this).toggleClass("active");
-    $("footer").addClass("active");
+    $("footer").toggleClass("active");
   });
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
-    $("#sidebar-wrapper").removeClass("active");
-    $(".menu-toggle").removeClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-     $("footer").removeClass("active");
-  });
+
+  
