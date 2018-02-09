9b52322154d319daf3cddf50efa4f9ea630ffdcc
Patricia Limcangco <plimcangco1@studentmail.nait.ca>
2018-02-08 20:39:13 -0700

    Update custom.js
    Added active class for footer

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -3,6 +3,7 @@ $(".menu-toggle").click(function(e) {
     $("#sidebar-wrapper").toggleClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     $(this).toggleClass("active");
+    $("footer").toggleClass("active");
   });
   // Closes responsive menu when a scroll trigger link is clicked
   $('#sidebar-wrapper .js-scroll-trigger').click(function() {
